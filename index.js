7c71ee497b4385fc811dc99b1029be5907bccc5e
PRC <105520102+x0prc@users.noreply.github.com>
2024-10-15 11:57:45 +0530

    index.js code update

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-document.getElementById('analyzeBtn').addEventListener('click', async () => {
-    const peFilePath = document.getElementById('peFilePath').value;
-    const expectedHash = document.getElementById('expectedHash').value;
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), 
+            contextIsolation: true, 
+            enableRemoteModule: false, 
+            nodeIntegration: false 
+        }
+    });
+
+        const startUrl = isDev 
+        ? 'http://localhost:3000' 
+        : `file://${path.join(__dirname, '../build/index.html')}`; 
+
+    mainWindow.loadURL(startUrl);
+
     
-    try {
-        const result = await window.api.runAnalysis(peFilePath, expectedHash);
-        document.getElementById('output').innerText = result;
-    } catch (error) {
-        document.getElementById('output').innerText = error;
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
     }
+
+        mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit(); 
+    }
+});
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();     }
+});
+
+ipcMain.on('some-event', (event, arg) => {
+    console.log(arg);
+    event.reply('some-reply', 'Response from main process'); 
 });
