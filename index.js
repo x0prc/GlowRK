4c370ceca5fb1c4b55ead1c7a3c9ff2e453c4e98
PRC <105520102+x0prc@users.noreply.github.com>
2024-10-16 13:36:25 +0530

    minor bug fixes in index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const isDev = require('electron-is-dev');
 
-function createWindow() {
-  
-    const mainWindow = new BrowserWindow({
+let mainWindow;
+
+async function createWindow() {
+    
+    const isDev = (await import('electron-is-dev')).default;
+
+   
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'), 
-            contextIsolation: true, 
-            enableRemoteModule: false, 
-            nodeIntegration: false 
-        }
+            preload: path.join(__dirname, 'preload.js'),             
+            contextIsolation: true,
+            enableRemoteModule: false,          
+            nodeIntegration: false        }
     });
 
-        const startUrl = isDev 
+    
+    const startUrl = isDev 
         ? 'http://localhost:3000' 
         : `file://${path.join(__dirname, '../build/index.html')}`; 
-
     mainWindow.loadURL(startUrl);
 
-    
-    if (isDev) {
+       if (isDev) {
         mainWindow.webContents.openDevTools();
     }
 
@@ -32,18 +33,15 @@ function createWindow() {
     });
 }
 
+// Handle app lifecycle events
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit(); 
+        app.quit(); // Quit app when all windows are closed (except on macOS)
     }
 });
 app.on('activate', () => {
     if (mainWindow === null) {
-        createWindow();     }
-});
-
-ipcMain.on('some-event', (event, arg) => {
-    console.log(arg);
-    event.reply('some-reply', 'Response from main process'); 
+        createWindow(); // Recreate window on macOS if it's closed and activated again
+    }
 });
